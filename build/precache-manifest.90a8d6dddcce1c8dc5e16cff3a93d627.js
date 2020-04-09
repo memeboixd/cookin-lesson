29bc1c9625c63b11ff05d08220150768a2639d4d
memeboixd <43546762+memeboixd@users.noreply.github.com>
2020-04-09 22:28:44 +0200

    Add files via upload

---
+++ build/precache-manifest.90a8d6dddcce1c8dc5e16cff3a93d627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e949369110fa4bcb47805949d3aa06a4",
+    "url": "/cookin-lesson/index.html"
+  },
+  {
+    "revision": "e11691296592b3a472b5",
+    "url": "/cookin-lesson/static/css/main.9e471ba5.chunk.css"
+  },
+  {
+    "revision": "212b7f61979eb8362346",
+    "url": "/cookin-lesson/static/js/2.d35c647e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cookin-lesson/static/js/2.d35c647e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11691296592b3a472b5",
+    "url": "/cookin-lesson/static/js/main.f8d4b5fa.chunk.js"
+  },
+  {
+    "revision": "981367e94843f43d662f",
+    "url": "/cookin-lesson/static/js/runtime-main.41e8ac56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cookin-lesson/static/media/logo.5d5d9eef.svg"
+  }
+]);
